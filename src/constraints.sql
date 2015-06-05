3fd4e59beb9c510feafcdbfa727f600b41865e02
mjjc <mjjc@users.noreply.github.com>
2015-06-06 01:03:08 +0200

    Few fixes, views, triggers

--- src/constraints.sql
+++ src/constraints.sql
@@ -2,16 +2,25 @@
 -- constraints.sql
 --
 
---------------------------------------------------
---	Trigger for detecting cycle presence		--
-
+--
+-- Trigger for detecting cycle presence
+--
 CREATE TRIGGER category_cycle_insert BEFORE INSERT ON CATEGORY
 FOR EACH ROW WHEN (NEW.PARENT_ID IS NOT NULL)
 EXECUTE PROCEDURE category_find_cycles();
--- Inefficient for inserts (because the only forbidden case is self-reference)
 
 CREATE TRIGGER category_cycle_update BEFORE UPDATE ON CATEGORY
 FOR EACH ROW WHEN (NEW.PARENT_ID IS DISTINCT FROM OLD.PARENT_ID)
 EXECUTE PROCEDURE category_find_cycles();
 
---------------------------------------------------
+--
+-- Trigger validating coupons
+--
+CREATE TRIGGER order_coupon_insert BEFORE INSERT ON ORDER_COUPON
+FOR EACH ROW EXECUTE PROCEDURE order_coupon_validate_trigger();
+
+CREATE TRIGGER order_coupon_update BEFORE UPDATE ON ORDER_COUPON
+FOR EACH ROW EXECUTE PROCEDURE order_coupon_validate_trigger();
+
+CREATE TRIGGER order_product_removal AFTER DELETE ON ORDER_PRODUCT
+FOR EACH ROW EXECUTE PROCEDURE order_coupon_after_order_product_delete();
--- src/functions.sql
+++ src/functions.sql
@@ -2,13 +2,13 @@
 -- functions.sql
 --
 
---------------------------------------------------
---	Trigger for detecting cycle presence		--
-
-CREATE OR REPLACE FUNCTION category_find_cycles() RETURNS trigger AS
+--
+-- Trigger for detecting cycle presence
+--
+CREATE OR REPLACE FUNCTION category_find_cycles() RETURNS TRIGGER AS
 $$
 DECLARE
-	cr	INTEGER;	--	variable to traverse category
+	cr INTEGER; -- variable to traverse category
 BEGIN
 	cr = NEW.PARENT_ID;
 	WHILE (cr IS NOT NULL) LOOP
@@ -18,92 +18,88 @@ BEGIN
 			SELECT PARENT_ID INTO cr FROM CATEGORY WHERE CATEGORY_ID = cr;
 		END IF;
 	END LOOP;
-	--	Reached the end - OK
+	-- Reached the end - OK
 	RETURN NEW;
 END
-$$	LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
 
---------------------------------------------------
---	Function returning all ancestor categories	--
-
-CREATE OR REPLACE FUNCTION anc_category(id INTEGER) RETURNS 
+--
+-- Function returning all ancestor categories
+--
+CREATE OR REPLACE FUNCTION category_ancestors(id INTEGER) RETURNS 
 TABLE 
 (
-	ID 		INTEGER, 
-	NAME 	VARCHAR(255)
+	ID   INTEGER, 
+	NAME VARCHAR(255)
 )
 AS
 $$
+	WITH RECURSIVE cat(ID) AS
+	(
+		SELECT PARENT_ID FROM CATEGORY WHERE CATEGORY_ID = id
+		UNION ALL
+		SELECT c.PARENT_ID FROM cat JOIN CATEGORY c ON cat.ID = c.CATEGORY_ID
+	)
 
-WITH RECURSIVE cat(ID) AS
-(
-	SELECT PARENT_ID FROM CATEGORY WHERE CATEGORY_ID = id
-	UNION ALL
+	SELECT cat.*, c.NAME FROM cat JOIN CATEGORY c on cat.ID = c.CATEGORY_ID;
+$$
+LANGUAGE sql;
 
-	SELECT c.PARENT_ID FROM cat JOIN CATEGORY c ON cat.ID = c.CATEGORY_ID
-)
-
-SELECT cat.*, c.NAME FROM cat JOIN CATEGORY c on cat.ID = c.CATEGORY_ID;
-
-$$	LANGUAGE sql;
---------------------------------------------------
---Function returning all descendants categories	--
-
-CREATE OR REPLACE FUNCTION desc_category(id INTEGER) RETURNS 
+--
+-- Function returning all descendants categories
+--
+CREATE OR REPLACE FUNCTION category_descendants(id INTEGER) RETURNS 
 TABLE 
 (
-	ID 		INTEGER, 
-	NAME 	VARCHAR(255)
+	ID    INTEGER, 
+	NAME  VARCHAR(255)
 )
 AS
 $$
+	WITH RECURSIVE cat(ID) AS
+	(
+		SELECT CATEGORY_ID FROM CATEGORY WHERE PARENT_ID = id
+		UNION ALL
+		SELECT c.CATEGORY_ID FROM cat JOIN CATEGORY c ON cat.ID = c.PARENT_ID
+	)
+	
+	SELECT cat.*, c.NAME FROM cat JOIN CATEGORY c ON cat.ID = c.CATEGORY_ID;
+$$
+LANGUAGE sql;
 
-WITH RECURSIVE cat(ID) AS
-(
-	SELECT CATEGORY_ID FROM CATEGORY WHERE PARENT_ID = id
-	UNION ALL
-
-	SELECT c.CATEGORY_ID FROM cat JOIN CATEGORY c ON cat.ID = c.PARENT_ID
-)
-
-SELECT cat.*, c.NAME FROM cat JOIN CATEGORY c ON cat.ID = c.CATEGORY_ID;
-
-$$	LANGUAGE sql;
-
---------------------------------------------------
---	Function returning all leaf categories		--
-
-CREATE OR REPLACE FUNCTION leaf_category() RETURNS 
+--
+-- Function returning all leaf categories
+--
+CREATE OR REPLACE FUNCTION category_leaves() RETURNS 
 TABLE
 (
-	ID		INTEGER,
-	NAME 	VARCHAR(255)
+	ID   INTEGER,
+	NAME VARCHAR(255)
 )
 AS
 $$
+	WITH cat(ID, ct) AS
+	(
+		SELECT c.CATEGORY_ID, COUNT(ct.CATEGORY_ID) FROM category c LEFT JOIN category ct ON c.CATEGORY_ID = ct.PARENT_ID GROUP BY c.CATEGORY_ID
+	)
 
-WITH cat(ID, ct) AS
-(
-	SELECT c.CATEGORY_ID, COUNT(ct.CATEGORY_ID) FROM category c LEFT JOIN category ct ON c.CATEGORY_ID = ct.PARENT_ID GROUP BY c.CATEGORY_ID
-)
+	SELECT c.CATEGORY_ID, c.NAME FROM CATEGORY c JOIN cat ON c.CATEGORY_ID = cat.ID WHERE cat.ct = 0;
+$$
+LANGUAGE sql;
 
-SELECT c.CATEGORY_ID, c.NAME FROM CATEGORY c JOIN cat ON c.CATEGORY_ID = cat.ID WHERE cat.ct = 0;
-
-$$	LANGUAGE sql;
-
-
---------------------------------------------------
---	Aggregate multiplication function (makes
---	calculating the total discount easier)		--
+--
+-- Aggregate multiplication function
+-- (makes calculating the total discount easier)
+--
 
 CREATE OR REPLACE FUNCTION numeric_multiply(a NUMERIC, b NUMERIC) RETURNS NUMERIC AS
 $$
-	BEGIN
-
+BEGIN
 	RETURN a * b;
-
-	END
-$$	LANGUAGE plpgsql;
+END
+$$
+LANGUAGE plpgsql;
 
 CREATE AGGREGATE multiply(NUMERIC)
 (
@@ -112,73 +108,170 @@ CREATE AGGREGATE multiply(NUMERIC)
 	initcond = 1
 );
 
---------------------------------------------------
---	Function fetching current price for product	--
+CREATE OR REPLACE VIEW product_price_detail AS
+SELECT
+	p.*,
+	(
+		SELECT MIN(created_at) FROM product_price WHERE product_id=p.product_id AND created_at > p.created_at
+	) as ends_at
+FROM product_price p;
 
-CREATE OR REPLACE FUNCTION current_price(id INTEGER) RETURNS NUMERIC(6, 2) AS
-
-$$
-
-	SELECT price FROM product_price 
-	WHERE PRODUCT_ID = id AND CREATED_AT = (SELECT MAX(CREATED_AT) FROM product_price WHERE PRODUCT_ID = id);
-
-$$	LANGUAGE sql;
-
---------------------------------------------------
---	Function returning information about order.
---	Fetching the discounts is ugly but works
---	(under assumption that validity of said 
---	discounts is guaranteed by order-coupon
---	relation).									--
+--
+-- Function returning information about order.
+-- Fetching the discounts is ugly but works (under assumption that validity
+-- of said discounts is guaranteed by order-coupon relation)
+--
 
 CREATE OR REPLACE FUNCTION order_information(id INTEGER) RETURNS 
 TABLE
 (
-	NO				bigint,
-	NAME			VARCHAR(128),
-	QUANTITY		INTEGER,
-	"UNIT PRICE" 	NUMERIC(6, 2),
-	"TOTAL PRICE" 	NUMERIC(6,2),
-	DISCOUNT 		NUMERIC(3, 2)
+	no             BIGINT,
+	name           VARCHAR(128),
+	quantity       INTEGER,
+	unit_price     NUMERIC(6,2),
+	total_price    NUMERIC(6,2),
+	unit_weight    NUMERIC(6,2),
+	total_weight   NUMERIC(6,2),
+	DISCOUNT       NUMERIC(3,2)
 )
 AS
-
 $$
-	WITH 
-	tp(total, product_id) AS
-	(
-		SELECT multiply(1 - DISCOUNT), PRODUCT_ID 
-		FROM 
-		(
-		SELECT * FROM ORDER_COUPON oc JOIN COUPON co USING (COUPON_ID)
-		WHERE oc.ORDER_ID = id AND PRODUCT_ID IS NOT NULL
-		) x
-		GROUP BY PRODUCT_ID
-	),
-	tk(total, category_id) AS
-	(
-		SELECT multiply(1 - DISCOUNT), CATEGORY_ID FROM
-		(
-		SELECT * FROM ORDER_COUPON oc JOIN COUPON co USING (COUPON_ID) 
-		WHERE oc.ORDER_ID = id AND CATEGORY_ID IS NOT NULL
-		) x 
-		GROUP BY CATEGORY_ID
-	),
-	tc(total) AS
-	(
-		SELECT multiply(1 - DISCOUNT) FROM ORDER_COUPON OC JOIN COUPON co USING (COUPON_ID)
-		WHERE oc.ORDER_ID = id AND co.PRODUCT_ID IS NULL AND CATEGORY_ID IS NULL
-	)
+	SELECT
+		row_number() over ()                   as no,
+		p.name                                 as name,
+		op.quantity                            as quantity,
+		ppd.price                              as unit_price,
+		ppd.price * op.quantity                as total_price,
+		p.weight                               as unit_weight,
+		p.weight * op.quantity                 as total_weight,
+		1 - multiply(1-coalesce(c.discount,0)) as discount
+	FROM "order" o
+	JOIN order_product op USING (order_id)
+	JOIN product p USING (product_id)
+	JOIN product_price_detail ppd USING (product_id)
+	LEFT JOIN order_coupon oc ON (oc.order_id=o.order_id)
+	LEFT JOIN coupon c ON (c.coupon_id=oc.coupon_id)
+	WHERE
+		o.order_id=id AND
+		ppd.created_at <= o.created_at AND
+		(o.created_at < ppd.ends_at OR ppd.ends_at is null) AND
+		(c.category_id IS NULL OR p.category_id IN (
+			SELECT id FROM category_descendants(c.category_id))) AND
+		(c.product_id IS NULL OR p.product_id = c.product_id)
+	GROUP BY name, quantity, unit_price, total_price, unit_weight,
+		total_weight
+	ORDER BY name
+$$
+LANGUAGE sql;
+
+--
+-- Coupon validity
+--
+CREATE OR REPLACE FUNCTION order_coupon_validate(o_id INTEGER, c_id VARCHAR(128)) RETURNS VOID AS
+$$
+DECLARE
+	c RECORD;
+BEGIN
+	SELECT * FROM coupon WHERE coupon_id=c_id INTO c;
 	
+	IF c.client_id IS NOT NULL THEN
+		IF NOT EXISTS (
+			SELECT * FROM "order" 
+			WHERE "order".order_id=o_id AND client_id=c.client_id
+			) THEN RAISE EXCEPTION 'Coupon "%" does not belong to this client.', c_id;
+		END IF;
+	END IF;
+	
+	IF c.category_id IS NOT NULL THEN
+		IF NOT EXISTS (
+			SELECT * FROM order_product 
+			JOIN product USING (product_id)
+			WHERE
+				order_product.order_id=o_id AND
+				product.category_id IN
+				(
+					SELECT id FROM category_descendants(c.category_id)
+				)
+			) THEN RAISE EXCEPTION 'Coupon "%" cannot be applied to this order.', c_id;
+		END IF;
+	END IF;
+	
+	IF c.product_id IS NOT NULL THEN
+		IF NOT EXISTS (
+			SELECT * FROM order_product
+			WHERE
+				order_product.order_id=o_id AND
+				product_id=c.product_id
+			) THEN RAISE EXCEPTION 'Coupon "%" cannot be applied to this order.', c_id;
+		END IF;
+	END IF;
+	
+	IF NOT EXISTS (
+		SELECT * FROM "order"
+		WHERE
+			"order".order_id=o_id AND
+			created_at BETWEEN c.VALID_FROM AND
+			c.VALID_TO
+		) THEN RAISE EXCEPTION 'Coupon "%" has expired.', c_id;
+	END IF;
+END
+$$
+language plpgsql;
 
-	SELECT row_number() over (), p.NAME, op.QUANTITY, current_price(p.PRODUCT_ID), 
-	current_price(p.PRODUCT_ID) * op.QUANTITY, 1 - COALESCE(tp.total, 1) * COALESCE(tk.total, 1) * COALESCE(tc.total, 1)
-	FROM ORDER_PRODUCT op JOIN PRODUCT p USING (PRODUCT_ID) 
-	LEFT OUTER JOIN tp ON p.PRODUCT_ID = tp.PRODUCT_ID 
-	LEFT OUTER JOIN tk ON p.CATEGORY_ID = tk.CATEGORY_ID 
-	CROSS JOIN tc WHERE ORDER_ID = id;
+--
+-- Trigger validating inserts in order_coupon relation
+--
+CREATE OR REPLACE FUNCTION order_coupon_validate_trigger() RETURNS TRIGGER AS
+$$
+BEGIN
+	PERFORM order_coupon_validate(NEW.order_id, NEW.coupon_id);
+	RETURN NEW;
+END
+$$
+language plpgsql;
 
-$$	LANGUAGE sql;
+--
+-- Clean unused coupon entries when removing products from order
+--
+CREATE OR REPLACE FUNCTION order_coupon_after_order_product_delete() RETURNS TRIGGER AS
+$$
+DECLARE
+	coupon RECORD;
+BEGIN
+	FOR coupon IN (SELECT * FROM order_coupon WHERE order_id=OLD.order_id) LOOP
+	BEGIN
+		PERFORM order_coupon_validate(OLD.order_id, coupon.coupon_id);
+	EXCEPTION WHEN OTHERS THEN
+		DELETE FROM order_coupon WHERE coupon_id=coupon.coupon_id AND order_id=OLD.order_id;
+	END;
+	END LOOP;
+	RETURN OLD;
+END
+$$
+language plpgsql;
 
-
---------------------------------------------------
+--
+-- View that shows current state of each employee
+--
+CREATE OR REPLACE VIEW employee_detail AS
+SELECT * FROM employee
+JOIN employee_position USING (employee_id)
+JOIN position USING (position_id)
+WHERE since >= ALL (SELECT since FROM employee_position X WHERE X.employee_id=employee_id);
+--
+-- Function that finds the best store and seller to be assigned to the given order
+--
+CREATE OR REPLACE FUNCTION find_best_seller(order_id INTEGER) RETURNS INTEGER AS
+$$
+	SELECT employee_id FROM order_product
+	JOIN product_price USING (product_id)
+	JOIN product USING (product_id)
+	JOIN product_stock USING (product_id)
+	JOIN employee_detail USING (branch_id)
+	-- TODO: join employee's order and count them into result
+	WHERE order_id=1
+	GROUP BY employee_id
+	ORDER BY sum(weight * least(order_product.quantity, product_stock.quantity))
+	LIMIT 1;
+$$
+language sql;
